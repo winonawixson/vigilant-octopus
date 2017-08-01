f793deba462fb8db830dd8c45e14c7ab6c42f241
winonawixson <winonawixson@gmail.com>
2017-08-01 19:30:26 -0400

    initial commit.

---
+++ Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int minutes = 7;
+            string message = "Hi, I made a variable of " + minutes.ToString() + " minutes. #thrilling";
+
+            Console.WriteLine(message);
+        }
+    }
+}
